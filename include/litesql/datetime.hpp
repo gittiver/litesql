24f5f6278c7e80417686fd6b598f4de230756b76
tlaitinen <tlaitinen@users.sf.net>
2005-11-23 19:23:34 +0000

    TimeStruct-class and Date,Time,DateTime - implementations

--- include/litesql/datetime.hpp
+++ include/litesql/datetime.hpp
@@ -11,22 +11,48 @@
 #include "field.hpp"
 namespace litesql {
 using namespace std;
-
+/* holds struct tm */
+class TimeStruct {
+    struct tm mytm;
+public:
+    TimeStruct(time_t t=0);
+    int day() const;
+    int dayOfWeek() const;
+    int dayOfYear() const;
+    int month() const;
+    int year() const;
+    int hour() const;
+    int min() const;
+    int sec() const;
+    time_t timeStamp() const;
+    TimeStruct& setDay(int day);
+    TimeStruct& setMonth(int month);
+    TimeStruct& setYear(int year);
+    TimeStruct& setHour(int hour);
+    TimeStruct& setMin(int min);
+    TimeStruct& setSec(int sec);
+    TimeStruct& setTimeStamp(time_t t);
+};
+        
 /** holds date */
 class Date {
     time_t value;
 public:
     /** crops time of day to 00:00:00 */
-    Date(time_t t);
+    Date(time_t t=0);
     Date(int day, int month, int year);
-    int day();
-    int month();
-    int year();
-    time_t timeStamp();
+    int day() const;
+    int dayOfWeek() const;
+    int month() const;
+    int year() const;
+    time_t timeStamp() const;
+    TimeStruct timeStruct() const;
+    
     Date& setDay(int d);
     Date& setMonth(int m);
     Date& setYear(int y);
-    string asString(string format="%d.%m.%y");
+    Date& setTimeStamp(time_t t);
+    string asString(string format="%d.%m.%y") const;
 };
 /** holds time of day */
 class Time {
@@ -34,33 +60,39 @@ class Time {
     int value;
 public:
     Time(int hour, int min, int sec);
-    int hour();
-    int min();
-    int sec();
-    int secs();
+    int hour() const;
+    int min() const;
+    int sec() const;
+    int secs() const;
+    string asString(string format="%h:%m:%s") const;
+
     Time& setHour(int d);
     Time& setMin(int m);
     Time& setSec(int y);
     Time& setSecs(int secs);
-    string asString(string format="%h:%m:%s");
 };
 /** holds date and time of day */
 class DateTime {
     time_t value;
 public:
-    DateTime(time_t t);
-    int hour();
-    int min();
-    int sec();
-    time_t timeStamp();
+    DateTime(time_t t=0);
+    int day() const;
+    int month() const;
+    int year() const;
+    int hour() const;
+    int min() const;
+    int sec() const;
+    time_t timeStamp() const;
+    TimeStruct timeStruct() const;
+    string asString(string format="%d.%m.%y %h:%m:%s") const;
+
     DateTime& setDay(int d);
     DateTime& setMonth(int m);
     DateTime& setYear(int y);
     DateTime& setHour(int d);
     DateTime& setMin(int m);
     DateTime& setSec(int y);
-    
-    string asString(string format="%d.%m.%y %h:%m:%s");
+    Date& setTimeStamp(time_t t);
 };
 };
 
--- src/library/datetime.cpp
+++ src/library/datetime.cpp
@@ -4,81 +4,260 @@
  * 
  * See LICENSE for copyright information. */
 #include "litesql/datetime.hpp"
+#include "litesql/split.hpp"
 #include <time.h>
 using namespace std;
 namespace litesql {
+
+TimeStruct::TimeStruct(time_t t) {
+    if (!t) 
+        t = time(NULL);
+    localtime_r(&t, &mytm);
+}
+int TimeStruct::day() const {
+    return mytm.tm_mday;
+}
+int TimeStruct::dayOfWeek() const {
+    return mytm.tm_wday;
+}
+int TimeStruct::dayOfYear() const {
+    return mytm.tm_yday;
+}
+int TimeStruct::month() const {
+    return mytm.tm_mon + 1;
+}
+int TimeStruct::year() const {
+    return mytm.tm_year + 1900;
+}
+int TimeStruct::hour() const {
+    return mytm.tm_hour;
+}
+int TimeStruct::min() const {
+    return mytm.tm_min;
+}
+int TimeStruct::sec() const {
+    return mytm.tm_sec;
+}
+time_t TimeStruct::timeStamp() const {
+    return mktime(const_cast<struct tm*>(&mytm));
+}
+TimeStruct&  TimeStruct::setDay(int day) {
+    mytm.tm_mday = day;
+    return *this;
+}
+TimeStruct& TimeStruct::setMonth(int month) {
+    mytm.tm_mon = month - 1;
+    return *this;
+}
+TimeStruct& TimeStruct::setYear(int year) {
+    mytm.tm_year = year - 1900;
+    return *this;
+}
+TimeStruct& TimeStruct::setHour(int hour) {
+    mytm.tm_hour = hour;
+    return *this;
+}
+TimeStruct& TimeStruct::setMin(int min) {
+    mytm.tm_min = min;
+    return *this;
+}
+TimeStruct& TimeStruct::setSec(int sec) {
+    mytm.tm_sec = sec;
+    return *this;
+}
+TimeStruct& TimeStruct::setTimeStamp(time_t t) {
+    localtime_r(&t, &mytm);
+    return *this;
+}
+                  
 Date::Date(time_t t) : value(t) {
+    if (value == 0)
+        value = time(NULL);
 }
 Date::Date(int day, int month, int year) {
-    struct tm mytm;
-    mytm.tm_sec = mytm.tm_min = mytm.tm_hour = 0;
-    mytm.tm_mday = day;
-    mytm.tm_mon = month - 1;
-    mytm.tm_year = year - 1900;
-    mytm.tm_isdst = -1;
-    value = mktime(&mytm);
+    value = TimeStruct().setDay(day).setMonth(month).setYear(year)
+        .timeStamp();
 }
-int Date::day() {
-    struct tm mytm;
-    localtime_r(&value, &mytm);
+int Date::day() const {
+    return TimeStruct(value).day();
 }
-int Date::month() {
+int Date::dayOfWeek() const {
+    return TimeStruct(value).dayOfWeek();
 }
-int Date::year() {
+int Date::month() const {
+    return TimeStruct(value).month();
 }
-Date& Date::setDay(int d) {
+int Date::year() const {
+    return TimeStruct(value).year();
 }
-Date& Date::setMonth(int m) {
-}
-Date& Date::setYear(int y) {
-}
-string Date::asString(string format) {
-}
-Time::Time(int hour, int min, int sec) {
-}
-int Time::hour() {
-}
-int Time::min() {
-}
-int Time::sec() {
-}
-int Time::secs() {
+time_t Date::timeStamp() const {
     return value;
 }
-Time& Time::setHour(int d) {
+TimeStruct Date::timeStruct() const {
+    return TimeStruct(value);
+}
+Date& Date::setDay(int d) {
+    value = TimeStruct(value).setDay(d).timeStamp();
+    return *this;
+}
+Date& Date::setMonth(int m) {
+    value = TimeStruct(value).setMonth(m).timeStamp();
+    return *this;
+}
+Date& Date::setYear(int y) {
+    value = TimeStruct(value).setYear(y).timeStamp();
+    return *this;
+}
+Date& Date::setTimeStamp(time_t t) {
+    value = t;
+    return *this;
+}
+string Date::asString(string format) const {
+    Split data(format, "%");
+    TimeStruct ts(value);
+    string res = data[0];
+    for (int i = 1; i < data.size(); i+=2) {
+        string rest = data[i].substr(1, data[i].size());
+        switch(data[i][0]) {
+        case 'd':
+            res += toString(ts.day()) + rest;
+            break;
+        case 'm':
+            res += toString(ts.month()) + rest;
+            break;
+        case 'y':
+            res += toString(ts.year()) + rest;
+        }
+        if (data.size() > i+1)
+            res += data[i+1];
+    }
+    return res;
+}
+Time::Time(int hour, int min, int sec) {
+    value = hour * 3600 + min * 60 + sec;
+}
+int Time::hour() const {
+    return value / 3600;
+}
+int Time::min() const {
+    return (value / 60) % 60;
+}
+int Time::sec() const {
+    return value % 60;
+}
+int Time::secs() const {
+    return value;
+}
+Time& Time::setHour(int h) {
+    value = h * 3600 + min() * 60 + sec();
+    return *this;
 }
 Time& Time::setMin(int m) {
+    value = hour() * 3600 + m * 60 + sec();
+    return *this;
 }
-Time& Time::setSec(int y) {
+Time& Time::setSec(int s) {
+    value = hour() * 3600 + min() * 60 + s;
+    return *this;
 }
 Time& Time::setSecs(int secs) {
+    value = secs;
+    return *this;
 }
-string Time::asString(string format) {
+string Time::asString(string format) const {
+    Split data(format, "%");
+    string res = data[0];
+    for (int i = 1; i < data.size(); i+=2) {
+        string rest = data[i].substr(1, data[i].size());
+        switch(data[i][0]) {
+        case 'h':
+            res += toString(hour()) + rest;
+            break;
+        case 'M':
+            res += toString(min()) + rest;
+            break;
+        case 's':
+            res += toString(sec()) + rest;
+        }
+        if (data.size() > i+1)
+            res += data[i+1];
+    }
+    return res;
 }
 DateTime::DateTime(time_t t) {
+    value = t;
+    if (!value)
+        value = time(NULL);
 }
-int DateTime::hour() {
+int DateTime::hour() const {
+    return TimeStruct(value).hour();
 }
-int DateTime::min() {
+int DateTime::min() const {
+    return TimeStruct(value).min();
 }
-int DateTime::sec() {
+int DateTime::sec() const {
+    return TimeStruct(value).sec();
 }
-time_t DateTime::timeStamp() {
+time_t DateTime::timeStamp() const {
+    return TimeStruct(value).timeStamp();
+}
+TimeStruct DateTime::timeStruct() const {
+    return TimeStruct(value);
 }
 DateTime& DateTime::setDay(int d) {
+    value = TimeStruct(value).setDay(d).timeStamp();
+    return *this;
 }
 DateTime& DateTime::setMonth(int m) {
+    value = TimeStruct(value).setMonth(m).timeStamp();
+    return *this;
 }
 DateTime& DateTime::setYear(int y) {
+    value = TimeStruct(value).setYear(y).timeStamp();
+    return *this;
 }
-DateTime& DateTime::setHour(int d) {
+DateTime& DateTime::setHour(int h) {
+    value = TimeStruct(value).setHour(h).timeStamp();
+    return *this;
 }
 DateTime& DateTime::setMin(int m) {
+    value = TimeStruct(value).setMin(m).timeStamp();
+    return *this;
 }
-DateTime& DateTime::setSec(int y) {
+DateTime& DateTime::setSec(int s) {
+    value = TimeStruct(value).setSec(s).timeStamp();
+    return *this;
 }
-string DateTime::asString(string format) {
-
+string DateTime::asString(string format) const {
+    Split data(format, "%");
+    TimeStruct ts(value);
+    string res = data[0];
+    for (int i = 1; i < data.size(); i+=2) {
+        string rest = data[i].substr(1, data[i].size());
+        switch(data[i][0]) {
+        case 'd':
+            res += toString(ts.day()) + rest;
+            break;
+        case 'm':
+            res += toString(ts.month()) + rest;
+            break;
+        case 'y':
+            res += toString(ts.year()) + rest;
+            break;
+        case 'h':
+            res += toString(ts.hour()) + rest;
+            break;
+        case 'M':
+            res += toString(ts.min()) + rest;
+            break;
+        case 's':
+            res += toString(ts.sec()) + rest;
+            break;
+        }
+        if (data.size() > i+1)
+            res += data[i+1];
+    }
+    return res;
 }
 
 }
