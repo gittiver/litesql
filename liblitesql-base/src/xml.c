d8274c2f4e90ec1a2f58bd78eadf524225dee6ac
tlaitinen <tlaitinen@users.sf.net>
2007-09-14 19:56:30 +0000

    object tag parsed

--- liblitesql-base/src/xml.c
+++ liblitesql-base/src/xml.c
@@ -10,6 +10,8 @@ typedef struct {
     xmlNode* root;
 
     lsqlDbDef* db;
+    
+
 
 } Parser;
 
@@ -25,6 +27,89 @@ static int getAttr(lsqlString* dst, xmlNode* node, const char* attr) {
     
     xmlFree(data);
     return 0;
+}
+static int getBool(lsqlBool* dst, xmlNode* node, const char* attr) {
+    xmlChar* data = xmlGetProp(node, (unsigned char*) attr);
+    if (!data)
+        return 0;
+
+    if (xmlStrcmp(data, (const xmlChar*) "true"))
+        *dst = 1;
+    else
+        *dst = 0;
+           
+    xmlFree(data);
+    return 0;
+}
+
+
+static int addToArray(void** array, size_t* size, size_t elemSize, void** elem) {
+
+    (*size)++;
+    *array = realloc(*array, elemSize * (*size));
+   
+    if (!*array) {
+        *elem = NULL;
+        return LSQL_MEMORY;
+    }
+
+    *elem = &((char*)*array)[((*size)-1)*elemSize];    
+    memset(*elem, 0, sizeof(elemSize));
+    return 0;
+}
+
+static int forNodes(Parser* p, void* ctx, xmlNode* first, 
+                    int (*func)(Parser*, void*, xmlNode*)) {
+
+    xmlNode* node = first;
+    while (node != NULL) {
+        int ret = func(p, ctx, node);
+        if (ret) {
+            return ret;
+        }
+        node = node->next;
+    }
+    return 0;
+}
+
+static int parseObjDef(Parser* p, xmlNode* node, lsqlObjDef* obj) {
+    int ret = 0;
+    ret |= getAttr(&obj->name, node, "name");
+    ret |= getBool(&obj->temporary, node, "temporary");
+    ret |= getAttr(&obj->inherits, node, "inherits");
+
+    return ret;
+}
+
+static int parseRelDef(Parser* p, xmlNode* node, lsqlRelDef* rel) {
+    printf("Parsing rel def %s\n", node->name);
+    return 0;
+}
+
+static int parseDbDefNodes(Parser* p, void* ctx, xmlNode* node) {
+    printf("parseDbDefNodes %s\n", node->name);
+    if (xmlStrcmp(node->name, (const xmlChar*) "object") == 0) {
+        lsqlObjDef* obj;
+        int ret = addToArray((void**) &p->db->objects, &p->db->objectsSize, 
+                   sizeof(lsqlObjDef), (void**) &obj);
+        if (ret)
+            return ret;
+
+        return parseObjDef(p, node, obj);
+    
+    } else if (xmlStrcmp(node->name, (const xmlChar*) "relation") == 0) {
+        lsqlRelDef* rel;
+        int ret = addToArray((void**) &p->db->relations, &p->db->relationsSize,
+                   sizeof(lsqlRelDef), (void**) &rel);
+        if (ret)
+            return ret;
+
+        return parseRelDef(p, node, rel);
+    
+    } else if (xmlStrcmp(node->name, (const xmlChar*) "include") == 0) {
+        
+        
+    }
 }
 
 int lsqlOpenDbDef(lsqlDbDef* def, const char* path) {
@@ -55,9 +140,14 @@ int lsqlOpenDbDef(lsqlDbDef* def, const char* path) {
     lsqlStringNew(&def->include);
 
     parser.root = xmlDocGetRootElement(parser.doc);
+
     ret |= getAttr(&def->name, parser.root, "name");
     ret |= getAttr(&def->namespace_, parser.root, "namespace");
     ret |= getAttr(&def->include, parser.root, "include");
+
+    
+
+    forNodes(&parser, NULL, parser.root->children, parseDbDefNodes);
 
 
 finish:
