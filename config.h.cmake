151e3d92c6aabc585863017184141b588519e4a5
gulliver <gulliver@fargonauten.de>
2009-03-17 15:12:18 +0000

    merged into trunk - cmake generates and uses now its own config.h (generated from config.h.cmake)

--- config.h.cmake
+++ config.h.cmake
@@ -1,136 +1,138 @@
-/* config.h.  Generated from config.h.in by configure.  */
 /* config.h.in.  Generated from configure.ac by autoheader.  */
 
 /* Define to 1 if you have the `atof' function. */
-#define HAVE_ATOF 1
+#cmakedefine HAVE_ATOF
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#cmakedefine HAVE_DLFCN_H
 
 /* Define to 1 if you have the `gettimeofday' function. */
-#define HAVE_GETTIMEOFDAY 1
+#cmakedefine HAVE_GETTIMEOFDAY
 
 /* Define to 1 if you have the <inttypes.h> header file. */
-#define HAVE_INTTYPES_H 1
+#cmakedefine HAVE_INTTYPES_H
 
 /* Define to 1 if you have the 'mysqlclient' library (-lmysqlclient). */
-/* #undef HAVE_LIBMYSQLCLIENT */
+#cmakedefine HAVE_LIBMYSQLCLIENT
 
 /* Define to 1 if you have the `pq' library (-lpq). */
-/* #undef HAVE_LIBPQ */
+#cmakedefine HAVE_LIBPQ
 
 /* Define to 1 if you have the `sqlite3' library (-lsqlite3). */
-/* #undef HAVE_LIBSQLITE3 */
+#cmakedefine HAVE_LIBSQLITE3
 
 /* Define to 1 if you have the `localtime_r' function. */
-/* #undef HAVE_LOCALTIME_R */
+#cmakedefine HAVE_LOCALTIME_R
 
 /* Define to 1 if your system has a GNU libc compatible `malloc' function, and
    to 0 otherwise. */
-#define HAVE_MALLOC 1
+#cmakedefine HAVE_MALLOC
 
 /* Define to 1 if you have the <memory.h> header file. */
-#define HAVE_MEMORY_H 1
+#cmakedefine HAVE_MEMORY_H
 
 /* Define to 1 if you have the `memset' function. */
-#define HAVE_MEMSET 1
+#cmakedefine HAVE_MEMSET
 
 /* Define to 1 if you have the `mktime' function. */
-#define HAVE_MKTIME 1
+#cmakedefine HAVE_MKTIME
 
 /* Define to 1 if your system has a GNU libc compatible `realloc' function,
    and to 0 otherwise. */
-#define HAVE_REALLOC 1
+#cmakedefine HAVE_REALLOC
 
 /* Define to 1 if stdbool.h conforms to C99. */
-#define HAVE_STDBOOL_H 1
+#cmakedefine HAVE_STDBOOL_H
 
 /* Define to 1 if you have the <stdint.h> header file. */
-#define HAVE_STDINT_H 1
+#cmakedefine HAVE_STDINT_H
 
 /* Define to 1 if you have the <stdlib.h> header file. */
-#define HAVE_STDLIB_H 1
+#cmakedefine HAVE_STDLIB_H
 
 /* Define to 1 if you have the `strchr' function. */
-#define HAVE_STRCHR 1
+#cmakedefine HAVE_STRCHR
 
 /* Define to 1 if you have the `strdup' function. */
-#define HAVE_STRDUP 1
+#cmakedefine HAVE_STRDUP
 
 /* Define to 1 if you have the <strings.h> header file. */
-#define HAVE_STRINGS_H 1
+#cmakedefine HAVE_STRINGS_H
 
 /* Define to 1 if you have the <string.h> header file. */
-#define HAVE_STRING_H 1
+#cmakedefine HAVE_STRING_H
 
 /* Define to 1 if you have the `strrchr' function. */
-#define HAVE_STRRCHR 1
+#cmakedefine HAVE_STRRCHR
 
 /* Define to 1 if you have the `strstr' function. */
-#define HAVE_STRSTR 1
+#cmakedefine HAVE_STRSTR
 
 /* Define to 1 if you have the `strtof' function. */
-/*#define HAVE_STRTOF 1 */
+#cmakedefine HAVE_STRTOF
 
 /* Define to 1 if you have the `strtol' function. */
-#define HAVE_STRTOL 1
+#cmakedefine HAVE_STRTOL
 
 /* Define to 1 if you have the `strtoll' function. */
-/*#define HAVE_STRTOLL 1 */
+#cmakedefine HAVE_STRTOLL
 
 /* Define to 1 if you have the <sys/stat.h> header file. */
-#define HAVE_SYS_STAT_H 1
+#cmakedefine HAVE_SYS_STAT_H
 
 /* Define to 1 if you have the <sys/time.h> header file. */
-#define HAVE_SYS_TIME_H 1
+#cmakedefine HAVE_SYS_TIME_H
 
 /* Define to 1 if you have the <sys/types.h> header file. */
-#define HAVE_SYS_TYPES_H 1
+#cmakedefine HAVE_SYS_TYPES_H
 
 /* Define to 1 if you have the <unistd.h> header file. */
-/* #define HAVE_UNISTD_H 1 */
+#cmakedefine HAVE_UNISTD_H
 
 /* Define to 1 if the system has the type `_Bool'. */
-#define HAVE__BOOL 1
+#cmakedefine HAVE__BOOL
 
 /* Name of package */
-#define PACKAGE "litesql"
+#cmakedefine PACKAGE
 
 /* Define to the address where bug reports for this package should be sent. */
-#define PACKAGE_BUGREPORT "litesql-users@lists.sourceforge.net"
+#cmakedefine PACKAGE_BUGREPORT
 
 /* Define to the full name of this package. */
-#define PACKAGE_NAME "LiteSQL"
+#cmakedefine PACKAGE_NAME
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "LiteSQL 0.3.3"
+#cmakedefine PACKAGE_STRING
 
 /* Define to the one symbol short name of this package. */
-#define PACKAGE_TARNAME "litesql"
+#cmakedefine PACKAGE_TARNAME
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "0.3.3"
+#cmakedefine PACKAGE_VERSION
 
 /* Define to 1 if you have the ANSI C header files. */
-#define STDC_HEADERS 1
+#cmakedefine STDC_HEADERS
 
 /* Define to 1 if you can safely include both <sys/time.h> and <time.h>. */
-#define TIME_WITH_SYS_TIME 1
+#cmakedefine TIME_WITH_SYS_TIME
 
 /* Version number of package */
-#define VERSION "0.3.3"
+#cmakedefine VERSION
 
 /* Define to empty if `const' does not conform to ANSI C. */
-/* #undef const */
+#cmakedefine const
 
 /* Define to `__inline__' or `__inline' if that's what the C compiler
    calls it, or to nothing if 'inline' is not supported under any name.  */
 #ifndef __cplusplus
-/* #undef inline */
+#undef inline
 #endif
 
 /* Define to rpl_malloc if the replacement function should be used. */
-/* #undef malloc */
+#cmakedefine malloc
 
 /* Define to rpl_realloc if the replacement function should be used. */
-/* #undef realloc */
+#cmakedefine realloc
 
 /* Define to `unsigned int' if <sys/types.h> does not define. */
-/* #undef size_t */
+#cmakedefine size_t
