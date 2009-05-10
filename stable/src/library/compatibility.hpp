4431b3268f508220efe305c02d74dedb34d393d6
tlaitinen <tlaitinen@users.sf.net>
2009-05-10 21:41:03 +0000

    HAVE_foo defines should be set accordingly. otherwise, the redundant prototypes break the build

--- stable/src/library/compatibility.hpp
+++ stable/src/library/compatibility.hpp
@@ -1,16 +1,18 @@
 #ifndef _compatibility_hpp
 #define _compatibility_hpp
 #include "config.h"
+#ifdef WIN32 // TODO: HAVE_STRTOLL not defined even though strtoll is available on Linux
 #ifndef HAVE_STRTOLL
 long long int strtoll(const char *nptr, char **endptr, int base);
 #endif
+#endif
+#ifdef WIN32 // TODO: HAVE_STRTOF not defined even though strtof is available on Linux
 #ifndef HAVE_STRTOF
 float strtof (const char *nptr, char **endptr);
 #endif
-#ifndef HAVE_LOCALTIME_R
-#ifdef WIN32
-#include <time.h>
 #endif
+#ifndef HAVE_LOCALTIME_R
+#include <time.h>
 struct tm *localtime_r(const time_t *clock, struct tm *result);
 #endif
 
