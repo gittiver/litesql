df03fab33ad42cd1cdf6e449f8aa2401e157b535
gulliver <gulliver@fargonauten.de>
2009-07-29 11:49:54 +0000

    empty lines removed, spaces -> tab

--- release_it.sh
+++ release_it.sh
@@ -1,21 +1,18 @@
 #!/bin/sh
 
-HOST_SYSTEM=`uname -o`
+HOST_SYSTEM=`uname -s`
 HOST_ARCH=`uname -m`
-
-svn checkout https://litesql.svn.sourceforge.net/svnroot/litesql/branches/stable release 
 
 AM_BUILDDIR=build/autotools/$HOST_ARCH-$HOST_SYSTEM
 CMAKE_BUILDDIR=build/cmake/$HOST_ARCH-$HOST_SYSTEM
 
 mkdir -p $CMAKE_BUILDDIR
 cd $CMAKE_BUILDDIR
-cmake ../../../release
-make test package
+cmake -D WITH_DOC:bool=ON -D WITH_MYSQL:bool=ON -D WITH_SQLITE:bool=ON -D WITH_TEST:bool=ON ../../..
+make all test package
+cd ../../..
 
 mkdir -p $AM_BUILDDIR
 cd $AM_BUILDDIR
-../../../release/configure
+../../../configure MYSQL_CONFIG=mysql_config5
 make distcheck
-
-
