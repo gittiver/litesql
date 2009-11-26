162fe8bcd8821ab2f08fd2c17e42bd17c999076d
gulliver <gulliver@fargonauten.de>
2009-11-26 15:49:07 +0000

    source package also as zip, installer build corrected

--- release_it.bat
+++ release_it.bat
@@ -7,7 +7,8 @@ mkdir -p %CMAKE_BUILDDIR%
 
 cd %CMAKE_BUILDDIR%
 cmake -D WITH_DOCS:bool=ON -D WITH_MYSQL:bool=ON -D WITH_SQLITE:bool=ON -D WITH_TEST:bool=ON  %STARTDIR%
-devenv /rebuild Debug litesql.sln /project "RUN_TESTS.vcproj" /projectconfig Debug
+devenv /rebuild Debug litesql.sln /project "ALL_BUILD.vcproj" /projectconfig Debug
+devenv /rebuild Release litesql.sln /project "ALL_BUILD.vcproj" /projectconfig Debug
 devenv /rebuild Release litesql.sln /project "PACKAGE.vcproj" /projectconfig Release
 
 cpack --config CPackSourceConfig.cmake
