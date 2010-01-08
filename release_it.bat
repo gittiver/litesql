25b0571b96de95c2181aaeae8a81d90f2713b009
gulliver <gulliver@fargonauten.de>
2010-01-08 16:13:53 +0000

    initial value of option corrected (WITH_TEST -> WITH_TESTS)

--- release_it.bat
+++ release_it.bat
@@ -6,7 +6,7 @@ set STARTDIR=%CD%
 mkdir -p %CMAKE_BUILDDIR%
 
 cd %CMAKE_BUILDDIR%
-cmake -D WITH_DOCS:bool=ON -D WITH_MYSQL:bool=ON -D WITH_SQLITE:bool=ON -D WITH_TEST:bool=ON  %STARTDIR%
+cmake -D WITH_DOCS:bool=ON -D WITH_MYSQL:bool=ON -D WITH_SQLITE:bool=ON -D WITH_TESTS:bool=ON  %STARTDIR%
 devenv /rebuild Debug litesql.sln /project "ALL_BUILD.vcproj" /projectconfig Debug
 devenv /rebuild Release litesql.sln /project "ALL_BUILD.vcproj" /projectconfig Debug
 devenv /rebuild Release litesql.sln /project "PACKAGE.vcproj" /projectconfig Release
