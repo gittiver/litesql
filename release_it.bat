744ddcf6ea6a434a26db3028842f78819cb13921
gulliver <gulliver@fargonauten.de>
2011-06-15 09:59:47 +0000

    adapted to visual studio 10, /MT disabled

--- release_it.bat
+++ release_it.bat
@@ -1,10 +1,16 @@
+set PATH=%PATH%;D:\Program Files\CMake 2.8\bin
 
 set CMAKE_BUILDDIR=build\cmake\vc
 set STARTDIR=%CD%
-set WXDIR=%STARTDIR%\..\..\wxWidgets\2.8.11
+set WXDIR=%STARTDIR%\..\wxWidgets\2.8.12
 
-@if "%VS71COMNTOOLS%"=="" call "%VS80COMNTOOLS%\vsvars32.bat"
-@if "%VS80COMNTOOLS%"=="" call "%VS71COMNTOOLS%\vsvars32.bat"
+
+@if NOT "%VS100COMNTOOLS%"=="" (call "%VS100COMNTOOLS%\vsvars32.bat" & goto VS_END)
+@if NOT "%VS90COMNTOOLS%"=="" (call "%VS90COMNTOOLS%\vsvars32.bat" & goto VS_END)
+@if NOT "%VS80COMNTOOLS%"=="" (call "%VS80COMNTOOLS%\vsvars32.bat" & goto VS_END)
+@if NOT "%VS71COMNTOOLS%"=="" (call "%VS71COMNTOOLS%\vsvars32.bat" & goto VS_END)
+
+:VS_END
 
 mkdir %CMAKE_BUILDDIR%
 
@@ -12,14 +18,14 @@ cd %CMAKE_BUILDDIR%
 
 del CMakeCache.txt
 
-cmake -D LITESQL_WITH_DOCS:bool=ON -D LITESQL_WITH_MYSQL:bool=ON -D LITESQL_WITH_SQLITE:bool=ON -D LITESQL_WITH_TESTS:bool=ON -D LITESQL_WITH_UI:bool=ON -D LITESQL_MSVC_MT:BOOL=ON -D wxWidgets_ROOT_DIR=%WXDIR% -D wxWidgets_LIB_DIR=%WXDIR%\lib\vc_lib %STARTDIR%
+cmake -D LITESQL_WITH_DOCS:bool=ON -D LITESQL_WITH_MYSQL:bool=ON -D LITESQL_WITH_SQLITE:bool=ON -D LITESQL_WITH_TESTS:bool=ON -D LITESQL_WITH_UI:bool=ON -D LITESQL_MSVC_MT:BOOL=OFF -D wxWidgets_ROOT_DIR=%WXDIR% -D wxWidgets_LIB_DIR=%WXDIR%\lib\vc_lib %STARTDIR%
 
-devenv /rebuild Debug litesql.sln /project "ALL_BUILD.vcproj" /projectconfig Debug
-devenv /rebuild Release litesql.sln /project "ALL_BUILD.vcproj" /projectconfig Debug
+devenv litesql.sln /Rebuild Debug /Project ALL_BUILD /ProjectConfig Debug
+devenv /Rebuild Release litesql.sln /Project ALL_BUILD /ProjectConfig Debug
 
-devenv /build Debug litesql.sln /project "RUN_TESTS.vcproj" /projectconfig Debug
+devenv /Build Debug litesql.sln /Project RUN_TESTS /ProjectConfig Debug
 
-devenv /build Release litesql.sln /project "PACKAGE.vcproj" /projectconfig Release
+devenv /Build Release litesql.sln /Project PACKAGE /ProjectConfig Release
 
 cpack --config CPackSourceConfig.cmake
 
