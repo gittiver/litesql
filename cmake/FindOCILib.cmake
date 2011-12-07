85a0d515ce352ccad91f25d56e7edf071e3ef58b
gulliver <gulliver@fargonauten.de>
2011-12-07 15:00:18 +0000

    check for PROGRAM_FILES32 before attempt to use in REPLACE

--- cmake/FindOCILib.cmake
+++ cmake/FindOCILib.cmake
@@ -17,7 +17,9 @@ else(OCILIB_INCLUDE_DIR AND OCILIB_LIBRARIES)
 
   # read environment variables and change \ to /
   SET(PROGRAM_FILES_32 $ENV{ProgramFiles})
+  if (${PROGRAM_FILES_32})
   STRING(REPLACE "\\\\" "/" PROGRAM_FILES_32 ${PROGRAM_FILES_32})
+  endif(${PROGRAM_FILES_32})
   
   SET(PROGRAM_FILES_64 $ENV{ProgramW6432})
   if (${PROGRAM_FILES_64})
