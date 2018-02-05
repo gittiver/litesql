0a8d5c155046a0dce304506d29aff48d6c5686e2
Gulliver <gulliver@traumkristalle.net>
2018-02-06 00:29:52 +0100

    added unique and indexed to gorm tags

--- src/generator/litesql-gen-golang.cpp
+++ src/generator/litesql-gen-golang.cpp
@@ -9,77 +9,93 @@ const char* GolangModuleGenerator::NAME = "golang-modulegenerator";
 
 static string toGolangType(AT_field_type field_type)
 {
-    switch(field_type)
-    {
+  switch(field_type)
+  {
     case A_field_type_integer:
-        return "int";
+      return "int";
     case A_field_type_float:
-        return "float";
+      return "float";
     case A_field_type_double:
-        return "double";
+      return "double";
     case A_field_type_boolean:
-        return "bool";
+      return "bool";
     case A_field_type_date:
-        return "date";
+      return "date";
     case A_field_type_time:
-        return "timestamp";
+      return "timestamp";
     case A_field_type_datetime:
-        return "datetime";
+      return "datetime";
     case A_field_type_blob:
-        return "blob";
+      return "blob";
     case A_field_type_string:
-        return "string";
+      return "string";
     default:
-        return "unknown";
-    }
+      return "unknown";
+  }
 }
 
 GolangGenerator::GolangGenerator(): CompositeGenerator(GolangGenerator::NAME)
 {
-    add(new GolangModuleGenerator());
-//  add(new RubyMigrationsGenerator());
+  add(new GolangModuleGenerator());
 }
 
 bool GolangModuleGenerator::generate(const xml::ObjectPtr& object)
 {
-    string fname = getOutputFilename(toLower(object->name + ".go"));
+  string fname = getOutputFilename(toLower(object->name + ".go"));
 
-    ofstream os(fname.c_str());
-    os  << "import (" << endl
-        << "\"github.com/jinzhu/gorm\"" << endl
-        << ")" << endl
-        << endl;
+  ofstream os(fname.c_str());
+  os  << "import (" << endl
+  << "\"github.com/jinzhu/gorm\"" << endl
+  << ")" << endl
+  << endl;
 
-    os  << "struct " << object->name  << "{" << endl
-        << "  gorm.Model" << endl;
+  os  << "struct " << object->name  << "{" << endl
+  << "  gorm.Model" << endl;
 
-    for (Field::sequence::const_iterator it = object->fields.begin(); it!= object->fields.end(); it++)
-    {
-        // TODO we want to generate for gorm, so we have to add tag conforming to gorm syntax
-        os << "  " << (*it)->name << " " << toGolangType((*it)->type)  << " ''" << endl;
+  for (Field::sequence::const_iterator it = object->fields.begin(); it!= object->fields.end(); it++)
+  {
+    // TODO we want to generate for gorm, so we have to add tag conforming to gorm syntax
+    os << "  " << (*it)->name << " " << toGolangType((*it)->type);
+    if ((*it)->isIndexed()||(*it)->isUnique()) {
+      os << " `gorm:\"";
+      bool semicolon = (*it)->isIndexed();
+      if ((*it)->isIndexed()) {
+        os << "index";
+      }
+      if ((*it)->isUnique()) {
+        if (semicolon){
+          os << ";";
+        }
+        os << "unique";
+      }
+      os << "\"`";
+
     }
 
-    for (RelationHandle::sequence::const_iterator it = object->handles.begin(); it!= object->handles.end(); it++)
-    {
+    os << endl;
+  }
 
-        os  << "  " << (*it)->name
-            << ((*it)->relate->hasLimit() ? "" : " []");
+  for (RelationHandle::sequence::const_iterator it = object->handles.begin(); it!= object->handles.end(); it++)
+  {
 
- //       if (!(*it)->name.empty())
- //           os << ", :through => :" << (*it)->name;
+    os  << "  " << (*it)->name
+    << ((*it)->relate->hasLimit() ? "" : " []");
+
+    //       if (!(*it)->name.empty())
+    //           os << ", :through => :" << (*it)->name;
 
 
-        os  << endl;
+    os  << endl;
 
-    }
+  }
 
-    os << "}" << endl;
-    os.close();
-    return true;
+  os << "}" << endl;
+  os.close();
+  return true;
 }
 
 bool GolangModuleGenerator::generateCode(const ObjectModel* model)
 {
-    CodeGenerator::generate(model->objects);
-    return true;
+  CodeGenerator::generate(model->objects);
+  return true;
 }
