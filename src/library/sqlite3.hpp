95adb291119ca56eb30ff2cda8d2afcb54abe0ea
Gulliver <gulliver@traumkristalle.net>
2018-04-13 07:28:10 +0200

    backend loading for sqlite3 on OSX

--- src/library/sqlite3.hpp
+++ src/library/sqlite3.hpp
@@ -35,7 +35,7 @@ namespace litesql {
         class Cursor;
         class Result;
 
-        SQLite3(const std::string& database);
+        SQLite3(const std::string& database) throw(DatabaseError);
         virtual ~SQLite3();
 
         virtual bool supportsSequences() const;
