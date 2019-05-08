f7b1c1cbb9c95c111b909f050c8f69c47d8ff8e4
Gulliver <gulliver@traumkristalle.net>
2019-05-08 23:26:11 +0200

    simplified script by direct usage of cmake calls

--- release_it.sh
+++ release_it.sh
@@ -4,7 +4,7 @@ HOST_SYSTEM=`uname -s`
 HOST_ARCH=`uname -m`
 
 AM_BUILDDIR=build/autotools/$HOST_ARCH-$HOST_SYSTEM
-CMAKE_BUILDDIR=build/cmake/$HOST_ARCH-$HOST_SYSTEM
+CMAKE_BUILDDIR=build-$HOST_ARCH-$HOST_SYSTEM
 STARTDIR=`pwd`
 
 if [ "$HOST_SYSTEM" = "Linux" ] 
@@ -12,20 +12,21 @@ then MYSQL_CONFIG=mysql_config
 else MYSQL_CONFIG=/opt/local/lib/mysql56/bin/mysql_config
 fi
 
-mkdir -p $CMAKE_BUILDDIR
+if [ "$HOST_SYSTEM" = "Darwin" ] 
+then cmake -S . -B build-xcode -D LITESQL_WITH_DOCS:bool=ON -D LITESQL_WITH_MYSQL:bool=ON -D LITESQL_WITH_SQLITE3:bool=ON -D LITESQL_WITH_TESTS:bool=ON -D LITESQL_WITH_UI:bool=OFF -G "Xcode"
+fi
 
-cd $CMAKE_BUILDDIR
-rm CMakeCache.txt
-cmake -D LITESQL_WITH_DOCS:bool=ON -D LITESQL_WITH_MYSQL:bool=ON -D LITESQL_WITH_SQLITE3:bool=ON -D LITESQL_WITH_TESTS:bool=ON -D LITESQL_WITH_UI:bool=OFF $STARTDIR
-cmake --build . --target all 
-cmake --build . --target all 
+rm $CMAKE_BUILDDIR/CMakeCache.txt
+cmake -S . -B $CMAKE_BUILDDIR -D LITESQL_WITH_DOCS:bool=ON -D LITESQL_WITH_MYSQL:bool=ON -D LITESQL_WITH_SQLITE3:bool=ON -D LITESQL_WITH_TESTS:bool=ON -D LITESQL_WITH_UI:bool=OFF
+cmake --build . --target all
+cmake --build . --target doc
+cmake --build . --target test
 cmake --build . --target package 
 cmake --build . --target dist
 #cmake --build . --target package_source
-cd $STARTDIR
 
-mkdir -p $AM_BUILDDIR
-cd $AM_BUILDDIR
-$STARTDIR/configure MYSQL_CONFIG=$MYSQL_CONFIG
-make distcheck
-cd $STARTDIR
+#mkdir -p $AM_BUILDDIR
+#cd $AM_BUILDDIR
+#$STARTDIR/configure MYSQL_CONFIG=$MYSQL_CONFIG
+#make distcheck
+#cd $STARTDIR
