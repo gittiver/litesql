6f594f486eb916ed0ba1a8e2ee4cee9de89df1cc
Gulliver <gulliver@traumkristalle.net>
2023-03-21 16:05:31 +0100

    fixed plugin loading test

--- src/library/sqlite3/sqlite3_backend.cpp
+++ src/library/sqlite3/sqlite3_backend.cpp
@@ -110,7 +110,7 @@ SQLite3::SQLite3(const string& connInfo) noexcept(false) //throw(DatabaseError)
 , beginTrans("BEGIN")
 {
   vector<string> params = split(connInfo,";");
-  string database = ":memory:";
+  string database;
   for (size_t i = 0; i < params.size(); i++) {
     vector<string> param = split(params[i], "=");
     if (param.size() == 1)
